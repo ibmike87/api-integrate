84de8f6be71994d025081a4e856d19ed0fe7ac1b
ibmike87 <ibmike87@gmail.com>
2021-04-23 01:27:50 +0900

    4장 API 연동하기  4-5. Context 와 함께 사용하기 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import Users from './Users';
+//import Users from './Users';
 import NewUsers from "./NewUsers";
+import {UsersProvider} from "./UsersContext";
 
 function App() {
   //직접 만든 커스텀 Hook 으로 동작하는 경우
   // return <Users />;
 
   //react-async 로 동작하는 경우
-  return <NewUsers />;
+  // return <NewUsers />;
+
+  //Context 와 함께 사용하기
+  return (
+      <UsersProvider>
+        <NewUsers />
+      </UsersProvider>
+  );
 }
 
 export default App;
--- src/NewUsers.js
+++ src/NewUsers.js
@@ -1,31 +1,41 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-import { useAsync } from 'react-async';
+import { useUsersState, useUsersDispatch, getUsers } from "./UsersContext";
 import User from './User';
+
+// import axios from 'axios';
+// import { useAsync } from 'react-async';
 
 // useAsync 에서는 Promise 의 결과를 바로 data에 담기 때문에
 // 요청을 한 이후 response 에서 data 추출하여 반환하는 함수를 따로 만듬.
-async function getUsers() {
+/*async function getUsers() {
     const response = await axios.get('https://jsonplaceholder.typicode.com/users');
     return response.data;
-}
+}*/
 
 function NewUsers() {
     const [userId, setUserId] = useState(null);
+    const state = useUsersState();
+    const dispatch = useUsersDispatch();
+
+    const { data: users, loading, error } = state.users;
+    const fetchData = () => {
+        getUsers(dispatch);
+    };
+
 
     // 렌더링하는 시점이 아닌 사용자의 특정 인터랙션에 따라 API 를 호출하고 싶을 땐 promiseFn 대신 deferFn 을 사용하고, reload 대신 run 함수를 사용
-    const { data: users, error, isLoading, run } = useAsync({
-        deferFn: getUsers
-    });
+    // const { data: users, error, isLoading, run } = useAsync({
+    //     deferFn: getUsers
+    // });
 
-    if (isLoading)
+    if (loading)
         return <div>로딩중 .... </div>;
 
     if (error)
         return <div>에러가 발생했습니다.</div>;
 
     if (!users)
-        return <button onClick={run}>불러오기</button>;
+        return <button onClick={fetchData}>불러오기</button>;
 
     return (
         <>
@@ -39,7 +49,7 @@ function NewUsers() {
                 }
             </ul>
 
-            <button onClick={run}>다시 불러오기</button>
+            <button onClick={fetchData}>다시 불러오기</button>
             {userId && <User id={userId} />}
         </>
     );
